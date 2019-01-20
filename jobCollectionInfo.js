be4e2542cc7847365a4d3681ef0f5497c7929445
gksapori <gerrysaporito@gmail.com>
2019-01-19 22:44:34 -0500

    improved UI

--- jobCollectionInfo.js
+++ jobCollectionInfo.js
@@ -25,25 +25,28 @@ jobs.push(locations);
 
 var html = "";
 html += "<!DOCTYPE html>\n<html lang='en' dir='ltr'>\n<head>\n<title>Waterloo Shortlist Table</title>\n<link rel='stylesheet' type='text/css' href='./shortlists.css'>\n</head>\n<body>\n";
-html += "<div class='main'>\n<h1>Prospective Jobs Table</h1>"
+html += "<div class='main'>\n<h1>Prospective WaterlooWorks Jobs Table</h1>"
 html += "<table>\n<tr class='titles'>\n";
-html += "<td><strong>Position</strong></td>\n<td><strong>Company</stong></td>\n<td><strong>Location</strong></td>\n<td></td>\n<td></td>\n<td></td>\n</tr>\n";
+html += "<td><strong>CV</strong</td><td><strong>Position</strong></td>\n<td><strong>Company</stong></td>\n<td><strong>Location</strong></td>\n<td></td>\n<td></td>\n<td></td>\n<td></td>\n<td></td>\n</tr>\n";
 for(let i = 0; i < jobs[0].length; i++) {
   html += "<tr>\n";
+  html += "<td><div class='check hidden'></div></td>\n";
   html += "<td>" + jobs[0][i] + "</td>\n";
   html += "<td>" + jobs[1][i] + "</td>\n";
   html += "<td>" + jobs[2][i] + "</td>\n";
   html += "<td><button class='save'>Save</button></td>\n"
   html += "<td><button class='maybe'>Maybe</button></td>\n"
   html += "<td><button class='remove'>Remove</button></td>\n"
-  html += "<td><button class='undo'>Undo</button></td>\n"
+  html += "<td><button class='undo hidden'>Undo</button></td>\n"
+  html += "<td><button class='cover'>CoverLetter</button></td>\n"
   html += "</tr>\n"
 }
 html += "</table>\n</div>\n<div id='counters'>\n"
 html += "<div id='counter'>\n<h2>Total Jobs: <span id='count'></span></h2>\n</div>\n"
 html += "<div class='counts'>\n<h4>Saved Jobs: <span id='good'></span></h4>\n</div>\n"
 html += "<div class='counts'>\n<h4>Maybe Jobs: <span id='ok'></span></h4>\n</div>\n"
-html += "<div class='counts'>\n<h4>Removed Jobs: <span id='bad'></span></h4>\n</div>\n</div>"
+html += "<div class='counts'>\n<h4>Removed Jobs: <span id='bad'></span></h4>\n</div>\n"
+html += "<div class='counts'>\n<h4>Jobs Left: <span id='unselected'></span></h4>\n</div>\n</div>"
 html += "<script type='text/javascript' src='./shortlists.js'></script>\n</body>\n</html>";
 
 console.log(html);
--- shortlists.js
+++ shortlists.js
@@ -1,78 +1,92 @@
+//Variables
 var save = document.querySelectorAll(".save"),
     maybe = document.querySelectorAll(".maybe"),
     remove = document.querySelectorAll(".remove"),
     undo = document.querySelectorAll(".undo"),
+    cover = document.querySelectorAll(".cover"),
     count = document.querySelector("#count"),
     good = document.querySelector("#good"),
     ok = document.querySelector("#ok"),
     bad = document.querySelector("#bad"),
+    unselected = document.querySelector("#unselected"),
     goodJobs = 0,
     okJobs = 0,
     badJobs = 0,
-    totalJobs = save.length;
+    totalJobs = save.length,
+    unselectedJobs = totalJobs;
 
 for(let i = 0; i < save.length; i++) {
+  //====================SAVE BUTTONS===========================
   save[i].addEventListener("click", function(){
     let row = save[i].parentElement.parentElement;
     //turn text-color green
     row.style.color = "green";
-    row.style.fontWeight = "bold";
     //hide save, maybe, and remove button button
     save[i].style.display = "none";
-    maybe[i].style.display = "none";
-    remove[i].style.display = "none";
+    maybe[i].style.display = "";
+    remove[i].style.display = "";
     //show undo
-    undo[i].classList.remove("undo");
+    undo[i].classList.remove("hidden");
     //increase good job count
     goodJobs++;
+    unselectedJobs--;
     //update counters
     updateCounters();
   });
 
+  //===================MAYBE BUTTONS=========================
   maybe[i].addEventListener("click", function(){
     let row = maybe[i].parentElement.parentElement;
     //turn text-color green
     row.style.color = "gainsboro";
     //hide save, maybe, and remove button button
-    save[i].style.display = "none";
+    save[i].style.display = "";
     maybe[i].style.display = "none";
-    remove[i].style.display = "none";
+    remove[i].style.display = "";
     //show undo
-    undo[i].classList.remove("undo");
+    undo[i].classList.remove("hidden");
     //increase ok job count
     okJobs++;
+    unselectedJobs--;
     //update counters
     updateCounters();
   });
 
+  //=======================REMOVE BUTTONS=======================
   remove[i].addEventListener("click", function(){
     let row = remove[i].parentElement.parentElement;
     //turn text-color white
     row.style.color = "rgba(0, 0, 0, 0)";
     //hide save, maybe, and remove button button
-    save[i].style.display = "none";
-    maybe[i].style.display = "none";
+    save[i].style.display = "";
+    maybe[i].style.display = "";
     remove[i].style.display = "none";
     //show undo
-    undo[i].classList.remove("undo");
+    undo[i].classList.remove("hidden");
     //increase bad job count
     badJobs++;
+    unselectedJobs--;
     //update counters
     updateCounters();
   });
 
+  //=======================UNDO BUTTONS========================
   undo[i].addEventListener("click", function(){
     let row = undo[i].parentElement.parentElement;
     //balance job count
     switch(row.style.color){
       case "green":
         goodJobs--;
+        unselectedJobs++;
         break;
       case "gainsboro":
         okJobs--;
+        unselectedJobs++;
         break;
       case "rgba(0, 0, 0, 0)":
         badJobs--;
+        unselectedJobs++;
+        break;
     }
     //turn text-color black and remove bold
     row.style.color = "black";
@@ -82,10 +96,23 @@ for(let i = 0; i < save.length; i++) {
     maybe[i].style.display = "";
     remove[i].style.display = "";
     //hide undo
-    undo[i].classList.add("undo");
+    undo[i].classList.add("hidden");
     //update counters
     updateCounters();
-  })
+  });
+
+  //================COVER LETTER BUTTONS=================
+  cover[i].addEventListener("click", function() {
+    let checked = cover[i].parentElement.parentElement.querySelector(".check");
+
+    if(checked.classList.contains("hidden")){
+      checked.classList.remove("hidden");
+      cover[i].style.backgroundColor = "RGB(190, 190, 190)";
+    } else {
+      checked.classList.add("hidden");
+      cover[i].style.backgroundColor = "#e4e4e4";
+    }
+  });
 }
 
 updateCounters();
@@ -95,4 +122,5 @@ function updateCounters() {
   good.innerHTML = goodJobs;
   ok.innerHTML = okJobs;
   bad.innerHTML = badJobs;
+  unselected.innerHTML = unselectedJobs;
 }
